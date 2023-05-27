4e96e4a170f1bcb72f1e64225c9b6d97c21ea971
Zavodny Petr <petr.zavodny@sensearena.com>
2023-05-27 11:07:40 +0200

    Up to lesson 33.

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -13,31 +13,50 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           body: Container(
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                colors: gradientColors,
-                center: Alignment.center,
-                radius: 1.3,
-                tileMode: TileMode.clamp,
-                stops: getGradientStops(),
-              ),
-            ),
+            decoration: getGradientLinearDecoration(),
             child: const Center(
-              child: Text('Hello World'),
+              child: Text(
+                'Hello World',
+                style: TextStyle(fontSize: 28, color: Colors.white),
+              ),
             ),
           ),
         ),
       ),
     );
 
-List<double> getGradientStops() {
+List<double> getGradientStops({start = 0, end = 1}) {
   List<double> stops = [];
 
   for (int i = 0; i < gradientColors.length; i++) {
-    double stop = 1 / (gradientColors.length - 1) * i;
+    double stop = start + (end - start) / (gradientColors.length - 1) * i;
     // debugPrint('stop: $stop');
     stops.add(stop);
   }
 
   return stops;
+}
+
+BoxDecoration getGradientLinearDecoration() {
+  return BoxDecoration(
+    gradient: LinearGradient(
+      colors: gradientColors,
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      tileMode: TileMode.clamp,
+      stops: getGradientStops(),
+    ),
+  );
+}
+
+BoxDecoration getGradientCircleDecoration() {
+  return BoxDecoration(
+    gradient: RadialGradient(
+      colors: gradientColors,
+      center: Alignment.center,
+      radius: 1.4,
+      tileMode: TileMode.clamp,
+      stops: getGradientStops(start: 0.3),
+    ),
+  );
 }
