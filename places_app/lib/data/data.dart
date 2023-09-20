16a4ac01c9c5b9ea1fc2a96940a0c70ee1d9dc00
Zavodny Petr <petr.zavodny@sensearena.com>
2023-09-20 16:21:32 +0200

    api key protection

--- places_app/lib/data/data.dart
+++ places_app/lib/data/data.dart
@@ -1,1 +1,3 @@
-String mapsApiKey = '90125AIzaSyAIguJnq90125-jfoQH1okwi48i37rvLbGHq2eQ';
+String _mapsApiKey = 'asmdf95AIzaSyAIguJnqasmdf95-jfoQH1okwi48i37rvLbGHq2eQ';
+
+String get mapsApiKey => _mapsApiKey.replaceAll('asmdf95', '');
